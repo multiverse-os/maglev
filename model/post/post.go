a5ac18ab70530802e369734fd9a8190bbccaed6b
Your Name <you@example.com>
2024-01-28 09:07:14 -0500

    it shouldn't be record, record is an what is stored, the model is what defines how and what is stored and what is allowed

--- model/post/post.go
+++ model/post/post.go
@@ -4,12 +4,14 @@ import "github.com/multiverse-os/webframe/model"
 
 // TODO: Record is naturally going to need to be tied to DB and collection but
 // knowing myself I already did that through the model.Record call
-type Record struct {
+type Model struct {
 	model.Record
 
 	Name        string
 	Description string
 }
+
+// TODO: So how do we create a simple model? This is the demonstration
 
 // TODO: I added attributes and methods, but with our record we don't need that
 // we could just add our attributes to the record itself and the same with
